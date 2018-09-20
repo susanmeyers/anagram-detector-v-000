151ab22a32d3af493fa2abbbfa42805ca03c41cc
Susan Meyers <smmeyers24@gmail.com>
2018-09-20 20:24:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,54 @@
-# Your code goes here!
+require 'pry'#
+class Anagram
+
+
+
+  attr_accessor :diaper, :ba
+
+  def initialize(diaper)
+    @diaper = diaper
+    @ba = ba
+  end
+
+  def match(diaper)
+    array = []
+    array.detect.sort
+  end
+
+
+  def match(ba)
+    #binding.pry
+    array = []
+    chars = array(ba).split("")
+    chars[2] = chars.length
+    array(ba).each do |char|
+
+  end
+  end
+
+
+
+  #
+  # end
+
+end
+
+
+# chars = input.split('')
+# puts chars.length
+# puts chars[2]
+# puts
+#
+#
+# chars.each { |c|
+#     puts c
+# }
+#
+#
+#
+#
+#
+#
+#
+#
+# #binding.pry
