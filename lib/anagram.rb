7d9a71bf4d2aeba8e0a88775150ba5d0505eedca
Susan Meyers <smmeyers24@gmail.com>
2018-09-21 07:12:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,51 +11,7 @@ class Anagram
   def match(possible_anagrams)
     possible_anagrams.select do |possible_anagram|
       @word.split("").sort == possible_anagram.split("").sort
-      
+
      end
   end
 end
-
-
-
-  # array.split("")
-  # array.each do |char|
-  #
-  # end
-
-
-  # def match(ba)
-  #   #binding.pry
-  #   array = []
-  #   chars = array(ba).split("")
-  #   chars[2] = chars.length
-  #   array(ba).each do |char|
-  #
-  # end
-  # end
-  #
-  #
-  #
-  # #
-  # # end
-
-
-
-# chars = input.split('')
-# puts chars.length
-# puts chars[2]
-# puts
-#
-#
-# chars.each { |c|
-#     puts c
-# }
-#
-#
-#
-#
-#
-#
-#
-#
-# #binding.pry
